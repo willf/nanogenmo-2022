a8525c38c8f5347bc22128298ac4af1df552ee92
Will Fitzgerald <willf@users.noreply.github.com>
2022-11-08 17:10:43 -0500

    wip

--- convert_modern_english.py
+++ convert_modern_english.py
@@ -1,15 +1,114 @@
+import json
 import os
+import re
+import sys
 
 import openai
+from bs4 import BeautifulSoup
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(
-  model="text-davinci-002",
-  prompt="Correct this to modern, idiomatic, plain English.\n\nNay but he himself rather is a liar from, the beginning,3 and so is any man whom he has suborned with his own <coin>, like Praxeas.",
-  temperature=0,
-  max_tokens=3887,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
+class ModernEnglishConverter():
+    def __init__(self, api_key=None, base_prompt='Correct this to modern, idiomatic, plain English.\n\n', max_tokens=4000):
+        if api_key:
+            openai.api_key = api_key
+        else:
+            openai.api_key = os.getenv("OPENAI_API_KEY")
+        self.base_prompt = base_prompt
+        self.max_tokens = max_tokens
+
+    def convert(self, text):
+        """Converts text to modern English."""
+        # log to stderr
+        print(f"Converting text: ->{text}<-", file=sys.stderr)
+
+        prompt = f"{self.base_prompt}{text}"
+
+        approximate_length = len(text) * 2 + len(self.base_prompt)
+        approximate_number_of_tokens_required = approximate_length // 4
+        if approximate_number_of_tokens_required > self.max_tokens:
+            raise ValueError(f"Text is too long. Approximate number of tokens required: {approximate_number_of_tokens_required}")
+
+        response = openai.Completion.create(
+          model="text-davinci-002",
+          prompt=prompt,
+          temperature=0,
+          max_tokens=self.max_tokens,
+          top_p=1,
+          frequency_penalty=0,
+          presence_penalty=0
+        )
+        d = {
+            'text': text,
+            'converted_text': response['choices'][0]['text'].strip(),
+            'completion': response
+            }
+        return d
+
+def squeeze(text):
+    """Squeezes a string by removing extra spaces and newlines."""
+    return re.sub(r'\s+', ' ', text).strip()
+
+def split_on_punctuation(text):
+    """Splits text on punctuation."""
+    return re.split(r'(?<=[.!?]) +', text)
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+
+def split_on_size(text, max_size=200):
+    """Split text into chunks of max_size."""
+    return [text[i:i+max_size] for i in range(0, len(text), max_size)]
+
+def split_text(text):
+    """splits text into sentences and then splits sentences into chunks of 1000 characters."""
+    return flatten([split_on_size(s) for s in split_on_punctuation(text)])
+
+def convert_html_text(html_text, pretty_print=True):
+    """Converts HTML text to modern English."""
+    soup = BeautifulSoup(html_text, 'html.parser')
+    converter = ModernEnglishConverter()
+    # walk the tree and convert each text node
+    for text_node in soup.find_all(text=True):
+        if text_node.parent.name not in ['p','li','h1','h2','h3','h4','h5','h6','i','b','strong','em','a']:
+            continue
+        text = squeeze(str(text_node))
+        if len(text) <= 20:
+            continue
+        splits = split_text(text)
+        # log splits to stderr
+        for s in splits:
+          print(f"Split text: ->{s}<-", file=sys.stderr)
+        replacement_texts = [converter.convert(s)['converted_text'] for s in splits]
+        replacement_text = ''.join(replacement_texts)
+        text_node.replace_with(replacement_text)
+    if pretty_print:
+        return soup.prettify()
+    return soup
+
+def convert_text(text):
+    """Converts text to modern English."""
+    converter = ModernEnglishConverter()
+    splits = split_text(text)
+    # log splits to stderr
+    for s in splits:
+      print(f"Split text: ->{s}<-", file=sys.stderr)
+    replacement_texts = [converter.convert(s)['converted_text'] for s in splits]
+    replacement_text = ''.join(replacement_texts)
+    return replacement_text
+
+
+
+if __name__ == '__main__':
+    converter = ModernEnglishConverter()
+    # collect text from stdin
+    text = ''
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        text += line
+    # convert text
+    d = convert_text(text)
+
+    print(d)
---
+++ openapi_completer.py
@@ -0,0 +1,123 @@
+import configparser
+import json
+import os
+import re
+import sys
+import textwrap
+import time
+
+import openai
+
+
+class ConfigReader():
+    def __init__(self, config_var, config_file='.env', default=None):
+        self.config_var = config_var
+        self.default = default
+        self.config_file = config_file
+
+    def read(self):
+        """
+        Reads an config variable.
+        It tries the following:
+        1. If the config variable is set, it returns the value.
+        2. If the config variable is not set, it tries to read it from a Config file.
+        3. If the config variable is not set and the Config file is not found, it used the default value.
+        4. Otherwise, it raises an exception.
+
+        """
+        value = os.getenv(self.config_var)
+        if value is not None:
+            return value
+        if self.config_file:
+            path = os.path.join(os.path.dirname(__file__), self.config_file)
+            config = configparser.ConfigParser()
+            config.read(path)
+            value = config['DEFAULT'][self.config_var]
+            if value is not None:
+                return value
+        if self.default is not None:
+            return self.default
+        raise Exception(f"Config variable {self.config_var} is not set.")
+
+class ChunkingCompleter():
+    def __init__(self, prompt_file='prompt.template',
+        variable_name='{{TEXT}}',
+        engine='text-davinci-002',
+        temperature=0.6,
+        top_p=1.0,
+        frequency_penalty=0.25,
+        presence_penalty=0.0,
+        stop=['<<END>>'],
+        max_tokens=3750,
+        chunk_size=1000,
+        max_tries=3):
+        openai.api_key = ConfigReader('OPENAI_API_KEY').read()
+        self.prompt_file = prompt_file
+        self.variable_name = variable_name
+        self.engine = engine
+        self.temperature = temperature
+        self.top_p = top_p
+        self.frequency_penalty = frequency_penalty
+        self.presence_penalty = presence_penalty
+        self.stop = stop
+        self.max_tokens = max_tokens
+        self.chunk_size = chunk_size
+        self.max_tries = max_tries
+
+    def get_prompt(self):
+        """Gets the prompt template."""
+        path = os.path.join(os.path.dirname(__file__), self.prompt_file)
+        with open(path) as f:
+            prompt = f.read()
+        return prompt
+
+    def chunk_text(self, text):
+        """Chunks text into chunks of size chunk_size."""
+        return textwrap.wrap(text, self.chunk_size)
+
+    def complete_chunk(self, chunk, prompt):
+        """Completes a chunk of text."""
+        tries = 1
+        while tries < self.max_tries:
+            tries += 1
+            try:
+                response = openai.Completion.create(
+                    model="text-davinci-002",
+                    prompt=prompt.replace(self.variable_name, chunk),
+                    temperature=self.temperature,
+                    top_p=self.top_p,
+                    frequency_penalty=self.frequency_penalty,
+                    presence_penalty=self.presence_penalty,
+                    stop=self.stop,
+                    max_tokens=self.max_tokens
+                )
+
+                completion_chunk = response['choices'][0]['text'].strip()
+                completion_chunk = re.sub('\s+', ' ', completion_chunk)
+                return {'completion': completion_chunk, 'success': True}
+            except Exception as e:
+                print(e)
+                sys.stderr.write('Error: {{}} Retrying... in {{}} seconds.\n'.format(e, tries-1))
+                sys.stderr.flush()
+                time.sleep(tries-1)
+        return {'completion': '', 'success': False}
+    def complete(self, text):
+        """Completes text."""
+        prompt = self.get_prompt()
+        chunks = self.chunk_text(text)
+        for chunk in chunks:
+            completion = self.complete_chunk(chunk, prompt)
+            result = {
+                'chunk': chunk,
+                'completion': completion["completion"],
+                'success': completion["success"]
+            }
+            yield result
+
+if __name__ == '__main__':
+    completer = ChunkingCompleter()
+    text = sys.stdin.read()
+    for result in completer.complete(text):
+        json.dump(result, sys.stdout)
+        sys.stdout.write('\n')
+        sys.stdout.flush()
